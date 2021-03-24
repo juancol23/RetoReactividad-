ce1376663db4241224d6c204148db1b357142128
DonValdemar <juan.colichon@vasslatam.com>
2021-03-24 13:27:27 -0500

    Personalizando propiedades

--- src/test/java/com/valdemar/demo/RetoClienteApplicationTests.java
+++ src/test/java/com/valdemar/demo/RetoClienteApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Reto01ApplicationTests {
+class RetoClienteApplicationTests {
 
 	@Test
 	void contextLoads() {
